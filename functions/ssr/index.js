9bbf90f600dfda9f509877923cc90479297cd869
Leonardo Matos <leomp120894@gmail.com>
2022-09-06 19:30:22 -0300

    Extract compiled Astro SSR handler
    Add `@cloudcommerce/storefront` as default SSR function dependecy
    
    https://github.com/ecomplus/cloud-commerce/commit/0370347fad1e03e0f5cb2bf9067dd8f438d8dd45

--- functions/ssr/index.js
+++ functions/ssr/index.js
@@ -1,9 +1,12 @@
 import config from '@cloudcommerce/firebase/config';
+import { handler as ssrHandler } from '../dist/server/entry.mjs';
 import mergeConfig from './config.json' assert { type: 'json' };
 
 config.set({
   ...mergeConfig,
   hello: 'from store',
 });
+
+global.ssr_handler = ssrHandler;
 
 export * from '@cloudcommerce/ssr/firebase';
