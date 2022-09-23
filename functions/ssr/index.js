68549f40bba99a46b30a9c5e0ac2116d0a89db53
Leonardo Matos <leomp120894@gmail.com>
2022-09-23 07:08:45 -0300

    Test SSR deploy with `minInstances: 1` (prevent cold starts)

--- functions/ssr/index.js
+++ functions/ssr/index.js
@@ -1,15 +1,14 @@
 import config from '@cloudcommerce/firebase/config';
+// eslint-ignore-next-line import/no-unresolved
 import { handler as ssrHandler } from './dist/server/entry.mjs';
 import mergeConfig from './config.json' assert { type: 'json' };
 
 config.set({
   ...mergeConfig,
   hello: 'from store',
-  /*
   httpsFunctionOptions: {
     minInstances: 1,
   },
-  */
 });
 
 global.ssr_handler = ssrHandler;
