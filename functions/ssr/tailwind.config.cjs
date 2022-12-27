660438d3990aed46677cab5315e08d53c4e238f3
Leonardo Matos <leomp120894@gmail.com>
2022-12-27 19:22:32 -0300

    Updating Tailwind/UnoCSS configs for theming [skip ci]
    With https://github.com/ecomplus/cloud-commerce/commit/a5673f0dfbb385bb47109c8221b07ee0b7c134ad

--- functions/ssr/tailwind.config.cjs
+++ functions/ssr/tailwind.config.cjs
@@ -3,4 +3,12 @@ process.env.STOREFRONT_BASE_DIR = __dirname;
 // eslint-disable-next-line import/no-extraneous-dependencies
 const defaultConfig = require('@cloudcommerce/storefront/tailwind.config.cjs');
 
-module.exports = defaultConfig;
+const themeOptions = {
+  // generalIcons: 'tabler',
+};
+const genTailwindConfig = () => defaultConfig.genTailwindConfig(themeOptions);
+
+module.exports = {
+  ...genTailwindConfig(),
+  themeOptions,
+};
--- functions/ssr/uno.config.cjs
+++ functions/ssr/uno.config.cjs
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { genUnoCSSConfig } = require('@cloudcommerce/storefront/uno.config');
+const { themeOptions } = require('./tailwind.config.cjs');
 
-export default genUnoCSSConfig();
+export default genUnoCSSConfig(themeOptions);
