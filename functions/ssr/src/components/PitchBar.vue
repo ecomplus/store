85bf28bc2ec5b17afd143a738e2d7be976527ffa
Leonardo Matos <leomp120894@gmail.com>
2023-02-04 23:58:01 -0300

    Setup `PitchBar` component with default styles
    Moving from https://github.com/ecomplus/cloud-commerce/commit/f9e45518f05bb0f603487b3637f60f0558d625fe
    
    [skip ci]

---
+++ functions/ssr/src/components/PitchBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="bg-base-100">
+    <div class="container md:w-2/3 mx-auto px-3 py-1">
+      <Carousel :autoplay="countValidSlides > 1 ? 7000 : null">
+        <li v-for="(slide, i) in slides" :key="i">
+          <component
+            :is="slide.href ? 'ALink' : 'span'"
+            :href="slide.href"
+            :target="slide.target"
+            class="inline-block px-8"
+            :class="slide.href ? 'hover:underline' : null"
+          >
+            <span
+              v-if="parsedContents[i]"
+              v-html="parsedContents[i]"
+              class="prose text-sm text-base-800"
+            ></span>
+          </component>
+        </li>
+        <template #controls>
+          <div
+            v-show="countValidSlides > 1"
+            class="text-xl leading-none text-base-400"
+          >
+            <CarouselControl
+              :direction="-1"
+              class="pr-2 bg-base-100 hover:text-base-700"
+            />
+            <CarouselControl class="pl-2 bg-base-100 hover:text-base-700" />
+          </div>
+        </template>
+      </Carousel>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import usePitchBar from '@@sf/composables/use-pitch-bar';
+import Carousel from '@@sf/components/Carousel.vue';
+import CarouselControl from '@@sf/components/CarouselControl.vue';
+
+export interface Props {
+  slides: Array<{
+    href?: string;
+    target?: string;
+    html: string;
+  }>;
+}
+
+const props = defineProps<Props>();
+const {
+  parsedContents,
+  countValidSlides,
+} = usePitchBar(props);
+</script>
