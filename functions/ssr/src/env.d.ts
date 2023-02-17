8a7ab7062d13bd40063c775feb77c468d9ff55b6
Leonardo Matos <leomp120894@gmail.com>
2023-02-17 20:44:19 -0300

    Add `@cloudcommerce/storefront/server` to end type declarations [skip ci]

--- functions/ssr/src/env.d.ts
+++ functions/ssr/src/env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="@astrojs/image/client" />
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="@cloudcommerce/storefront/client" />
+/// <reference types="@cloudcommerce/storefront/server" />
