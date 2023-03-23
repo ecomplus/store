f0ed20fe4d23d963eef63eb376162547f93e17d2
Leonardo Matos <leomp120894@gmail.com>
2023-03-23 14:01:47 -0300

    Add build script as workaround for cloudcommerce pnpm monorepo

---
+++ functions/ssr/scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# workaround for cloudcommerce pnpm monorepo
+filename="../../../packages/storefront/scripts/build-prod.sh"
+if [[ -f $filename ]]; then
+  sh $filename
+elif [[ -f "../$filename" ]]; then
+  sh "../$filename"
+else
+  echo '> npx storefront build'
+  npx storefront build
+fi
