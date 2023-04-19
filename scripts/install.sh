559b86a4bebd498d878682ad33c5902be1c9e572
Leonardo Matos <leomp120894@gmail.com>
2023-04-19 12:12:13 -0300

    Edit install.sh script to also CI root dev deps if not ready
    Fixes dve container (Codespaces) setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo '> Install functions dependencies:'
+if [ ! -d "./node_modules" ]; then
+  echo '>> Install root dev dependencies:'
+  npm ci
+fi
+echo '>> Install each function dependencies:'
 cd functions
 for d in */ ; do
   cd $d
   npm ci
   cd ..
 done
+cd ..
