3a8eb477efd3b2930d788f72d964f2b48a0346aa
Leonardo Matos <leomp120894@gmail.com>
2023-04-21 17:05:28 -0300

    Better naming things [skip ci]

--- functions/ssr/astro.config.mjs
+++ functions/ssr/astro.config.mjs
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import astroConfig from '@cloudcommerce/storefront/astro.config.mjs';
+import definedAstroConfig from '@cloudcommerce/storefront/astro.config.mjs';
 
-export default astroConfig;
+export default definedAstroConfig;
