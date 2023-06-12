ce9e7bf310cde8444ede438b9ea65f9ee19836cf
Leonardo Matos <leomp120894@gmail.com>
2023-06-12 19:55:56 -0300

    Update config files and add new style.css
    Following https://github.com/ecomplus/cloud-commerce/commit/244a9e7023c4643ddb766ff7a6ef4f58baa248bb [skip ci]

--- functions/ssr/tailwind.config.cjs
+++ functions/ssr/tailwind.config.cjs
@@ -1,14 +1,13 @@
 process.env.STOREFRONT_BASE_DIR = __dirname;
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-const defaultConfig = require('@cloudcommerce/storefront/tailwind.config.cjs');
+const { genTailwindConfig } = require('@cloudcommerce/storefront/config/storefront.tailwind.cjs');
 
 const themeOptions = {
   // generalIcons: 'tabler',
 };
-const genTailwindConfig = () => defaultConfig.genTailwindConfig(themeOptions);
 
 module.exports = {
-  ...genTailwindConfig(),
+  ...genTailwindConfig(themeOptions),
   themeOptions,
 };
--- functions/ssr/uno.config.cjs
+++ functions/ssr/uno.config.cjs
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { genUnoCSSConfig } = require('@cloudcommerce/storefront/uno.config');
-const { themeOptions } = require('./tailwind.config.cjs');
+const { genUnoCSSConfig } = require('@cloudcommerce/storefront/config/storefront.unocss.cjs');
+const tailwindConfig = require('./tailwind.config.cjs');
 
-export default genUnoCSSConfig(themeOptions);
+export default genUnoCSSConfig(tailwindConfig);
