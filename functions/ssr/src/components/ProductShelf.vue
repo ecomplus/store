7caca3a3cccc04e79c52f07ae881c144d1f7cc57
Leonardo Matos <leomp120894@gmail.com>
2023-06-17 23:24:48 -0300

    Getting started with CMS configured sections
    Updating home main content with https://github.com/ecomplus/cloud-commerce/commit/ae709cda9920a3ca008c292ac57f659163eb5190
    
    [skip ci]

---
+++ functions/ssr/src/components/ProductShelf.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="product in products" :key="product._id">
+        {{ product.name }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script setup lang="ts">
+import {
+  type Props as UseProductShelfProps,
+  useProductShelf,
+} from '@@sf/composables/use-product-shelf';
+
+export interface Props extends UseProductShelfProps {}
+
+const props = defineProps<Props>();
+const { fetching, products } = useProductShelf(props);
+if (import.meta.env.SSR) {
+  await fetching.value;
+}
+</script>
