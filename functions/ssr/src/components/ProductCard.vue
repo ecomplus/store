ba898ab9b0619158bd4ff26825076257b424b866
Leonardo Matos <leomp120894@gmail.com>
2023-06-20 20:18:37 -0300

    Update home hero and section with https://github.com/ecomplus/cloud-commerce/commit/5cf529c2284ebace2e13670ba4dfb38dee79db4b
    Starting with product card component on shelf
    
    [skip ci]

---
+++ functions/ssr/src/components/ProductCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <article :data-sku="product.sku">
+    {{ product.name }} {{ product._id }}
+  </article>
+</template>
+
+<script setup lang="ts">
+import type { Products, SearchItems } from '@cloudcommerce/types';
+
+export interface Props {
+  product: Products | SearchItems[0];
+}
+
+defineProps<Props>();
+</script>
--- functions/ssr/src/components/ProductShelf.vue
+++ functions/ssr/src/components/ProductShelf.vue
@@ -1,8 +1,16 @@
 <template>
   <section>
+    <h2 v-if="title">
+      <ALink v-if="titleLink" :href="titleLink">
+        {{ title }}
+      </ALink>
+      <template v-else>
+        {{ title }}
+      </template>
+    </h2>
     <ul>
       <li v-for="product in products" :key="product._id">
-        {{ product.name }}
+        <ProductCard :product="product" />
       </li>
     </ul>
   </section>
@@ -13,12 +21,18 @@ import {
   type Props as UseProductShelfProps,
   useProductShelf,
 } from '@@sf/composables/use-product-shelf';
+import ProductCard from '~/components/ProductCard.vue';
 
 export interface Props extends UseProductShelfProps {}
 
 const props = defineProps<Props>();
-const { fetching, products } = useProductShelf(props);
+const {
+  title,
+  titleLink,
+  fetching,
+  products,
+} = useProductShelf(props);
 if (import.meta.env.SSR) {
-  await fetching.value;
+  await fetching;
 }
 </script>
