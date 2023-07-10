518bbf5576ee782db0367e5ada234f1ede86b25b
Leonardo Matos <leomp120894@gmail.com>
2023-07-10 16:23:48 -0300

    Fixing env.d.ts for Vue [skip ci]

--- functions/ssr/src/env.d.ts
+++ functions/ssr/src/env.d.ts
@@ -1,4 +1,13 @@
 /// <reference types="@astrojs/image/client" />
 /// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vue/ref-macros" />
 /// <reference types="@cloudcommerce/storefront/client" />
 /// <reference types="@cloudcommerce/storefront/server" />
+
+/* eslint-disable import/newline-after-import */
+
+declare module '*.vue' {
+  import { type DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
