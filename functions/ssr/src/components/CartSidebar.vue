a30b5a2f8cacb5754d315ae26329e14fb2574d78
Leonardo Matos <leomp120894@gmail.com>
2023-07-14 13:09:35 -0300

    Add free shipping CTA on cart sidebar

--- functions/ssr/src/components/CartSidebar.vue
+++ functions/ssr/src/components/CartSidebar.vue
@@ -8,13 +8,22 @@
         {{ totalItems }}
       </span>
     </header>
-    <article class="grow bg-base-50">
+    <article class="grow bg-base-50 border-t-2 border-b-2 border-base-100">
+      <div
+        v-if="freeShippingFromValue"
+        class="secondary-subtle p-3 text-sm text-center"
+      >
+        {{ $t.i19add$1ToEarn.replace('$1',
+          $money(freeShippingFromValue - shoppingCart.subtotal)) }}
+        <strong class="lowercase">{{ $t.i19freeShipping }}</strong>
+      </div>
     </article>
-    <footer class="px-6 py-4">
+    <footer class="p-5 shadow">
     </footer>
   </aside>
 </template>
 
 <script setup lang="ts">
-import { totalItems } from '@@sf/state/shopping-cart';
+import { freeShippingFromValue } from '@@sf/state/modules-info';
+import { shoppingCart, totalItems } from '@@sf/state/shopping-cart';
 </script>
