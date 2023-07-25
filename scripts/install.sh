a9ebf22580a46b61370a5a29b72366e5ca295a30
Leonardo Matos <leomp120894@gmail.com>
2023-07-25 14:31:29 -0300

    Fix install script to check lock file and prevent error with `npm ci`

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,13 +2,21 @@
 
 if [ ! -d "./node_modules" ]; then
   echo '>> Install root dev dependencies:'
-  npm ci
+  if [ -f "./package-lock.json" ]; then
+    npm ci
+  elif
+    npm i
+  fi
 fi
 echo '>> Install each function dependencies:'
 cd functions
 for d in */ ; do
   cd $d
-  npm ci
+  if [ -f "./package-lock.json" ]; then
+    npm ci
+  elif
+    npm i
+  fi
   cd ..
 done
 cd ..
