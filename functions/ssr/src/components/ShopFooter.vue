1f6288bff8f7fd8453282411c79fcd05891f045b
Leonardo Matos <leomp120894@gmail.com>
2023-08-17 19:44:26 -0300

    Start showing payment methods flags and other minor advances on shop footer

--- functions/ssr/src/components/ShopFooter.vue
+++ functions/ssr/src/components/ShopFooter.vue
@@ -1,35 +1,45 @@
 <template>
   <footer class="w-screen">
-    <ul class="flex flex-wrap gap-4 w-96">
-      <li v-for="(stamp, i) in stamps" :key="i">
-        <component
-          :is="stamp.href ? 'a-link' : 'span'"
-          :href="stamp.href?.replace('{domain}', $settings.domain || '')"
-        >
-          <slot :name="`picture-${i}`" />
-          <span v-if="!stamp.img" class="flex items-center">
-            <span v-if="stamp.icon" class="text-4xl text-success-400 mr-2">
-              <i v-if="stamp.icon === 'lock'" class="i-lock-closed-20-solid"></i>
-              <i v-else class="i-check-badge-20-solid"></i>
-            </span>
-            <span class="text-sm font-medium text-base-600 max-w-[140px]">
-              {{ stamp.alt }}
-              <i v-if="stamp.href" class="ml-0.5 i-arrow-top-right-on-square"></i>
-            </span>
-          </span>
-        </component>
-      </li>
-    </ul>
+    <div class="container mx-auto">
+      <div class="flex justify-between gap-4">
+        <ul class="flex flex-wrap gap-4">
+          <li v-for="(stamp, i) in stamps" :key="i">
+            <ALink :href="stamp.href?.replace('{domain}', $settings.domain || '')">
+              <slot :name="`picture-${i}`" />
+              <span v-if="!stamp.img" class="flex items-center">
+                <span
+                  v-if="stamp.icon"
+                  class="text-4xl text-success-400 mr-2"
+                >
+                  <i v-if="stamp.icon === 'lock'" class="i-lock-closed-20-solid"></i>
+                  <i v-else class="i-check-badge-20-solid"></i>
+                </span>
+                <span class="text-sm font-medium text-base-600 max-w-[140px]">
+                  {{ stamp.alt }}
+                  <i v-if="stamp.href" class="ml-0.5 i-arrow-top-right-on-square"></i>
+                </span>
+              </span>
+            </ALink>
+          </li>
+        </ul>
+        <div class="flex flex-wrap items-center gap-3 text-2xl">
+          <PaymentMethodFlag
+            v-for="paymentMethod in $settings.payment_methods"
+            :key="paymentMethod"
+            :flag="paymentMethod"
+          />
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
 <script setup lang="ts">
 import type { LayoutContent } from '@@sf/content';
+import PaymentMethodFlag from '@@sf/components/PaymentMethodFlag.vue';
 
 export interface Props {
-  serviceLinks?: LayoutContent['service_links'];
   stamps?: LayoutContent['footer']['stamps'];
-  paymentMethods?: LayoutContent['footer']['payment_methods'];
 }
 
 defineProps<Props>();
