bf7beb8c1ebc5a0d9b2798f95572ed002f1f9df4
Leonardo Matos <leomp120894@gmail.com>
2023-10-15 18:41:25 -0300

    New SKU selector component for variation choose by grid and options

---
+++ functions/ssr/src/components/SkuSelector.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div v-for="(options, gridId) in variationsGrids" :key="gridId">
+      <span class="text-base-700 text-sm font-medium">
+        {{ getGridTitle(gridId) }}:
+        <strong v-if="selectedOptions[gridId]" class="text-base-800">
+          {{ selectedOptions[gridId] }}
+        </strong>
+      </span>
+      <ul v-if="options.length < 7" class="mt-2 flex gap-2">
+        <li v-for="(optionText, i) in options" :key="`${gridId}-${i}`">
+          <button
+            class="ring-secondary/60 rounded border"
+            :class="[
+              selectedOptions[gridId] === optionText
+                ? 'border-secondary ring-2'
+                : null,
+              gridId === 'colors'
+                ? 'h-9 w-9 text-[0]'
+                : 'px-2 py-1',
+              activeVariationsGrids[gridId].includes(optionText)
+                ? 'text-base-800 bg-base-100 border-base-400'
+                : 'text-base-500 bg-base-200 border-base-300'
+            ]"
+            :style="gridId === 'colors' ? getColorOptionBg(optionText) : undefined"
+            @click="selectOption({ optionText, gridId, gridIndex: i })"
+          >
+            {{ optionText }}
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  type Props as UseSkuSelector,
+  useSkuSelector,
+} from '@@sf/composables/use-sku-selector';
+
+export interface Props extends UseSkuSelector {}
+
+const props = defineProps<Props>();
+const emit = defineEmits(['update:variationId']);
+const {
+  variationsGrids,
+  activeVariationsGrids,
+  selectOption,
+  selectedOptions,
+  variationId,
+  getGridTitle,
+  getColorOptionBg,
+} = useSkuSelector(props);
+watch(variationId, (_id) => {
+  emit('update:variationId', _id);
+});
+</script>
