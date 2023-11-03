8a0618accf55bbe5cc231f5ad6cbbbddf45436cb
Leonardo Matos <leomp120894@gmail.com>
2023-11-02 22:53:02 -0300

    Redirect to /app/ SPA after login on account page

--- functions/ssr/src/components/AccountPage.vue
+++ functions/ssr/src/components/AccountPage.vue
@@ -2,31 +2,27 @@
   <article class="from-secondary-700 to-secondary-950 flex
     min-h-screen w-full flex-col justify-center bg-gradient-to-br">
     <div class="mx-auto p-4">
-      <Fade>
-        <div v-if="!isLogged">
-          <section class="ui-section text-center">
-            <a href="/" class="text-base-200 hover:text-white">
-              <i class="i-arrow-right mr-1 rotate-180"></i>
-              {{ $t.i19goToStore }}
-            </a>
-          </section>
-          <section class="overflow-hidden rounded-md
-            shadow-md ring-4 ring-black/10">
-            <LoginForm>
-              <template #head>
-                <div class="mb-5 text-center">
-                  <a href="/" class="inline-block">
-                    <slot name="logo" />
-                  </a>
-                </div>
-              </template>
-            </LoginForm>
-          </section>
-          <nav
-            v-if="$settings.serviceLinks?.length"
-            class="ui-section grid grid-cols-1
-            items-center gap-3 text-center md:grid-cols-2"
-          >
+      <section class="ui-section text-center">
+        <a href="/" class="text-base-200 hover:text-white">
+          <i class="i-arrow-right mr-1 rotate-180"></i>
+          {{ $t.i19goToStore }}
+        </a>
+      </section>
+      <section class="overflow-hidden rounded-md
+        shadow-md ring-4 ring-black/10">
+        <LoginForm>
+          <template #head>
+            <div class="mb-5 text-center">
+              <a href="/" class="inline-block">
+                <slot name="logo" />
+              </a>
+            </div>
+          </template>
+        </LoginForm>
+      </section>
+      <nav v-if="$settings.serviceLinks?.length" class="ui-section">
+        <ul class="flex items-center justify-center gap-3">
+          <li class="basis-full text-center md:basis-1/2">
             <ALink
               v-for="({ title, href }, i) in $settings.serviceLinks"
               :key="`s-${i}`"
@@ -35,9 +31,9 @@
             >
               {{ title }}
             </ALink>
-          </nav>
-        </div>
-      </Fade>
+          </li>
+        </ul>
+      </nav>
     </div>
   </article>
 </template>
@@ -45,4 +41,12 @@
 <script setup lang="ts">
 import { isLogged } from '@@sf/state/customer-session';
 import LoginForm from '~/components/LoginForm.vue';
+
+watch(isLogged, () => {
+  if (isLogged.value) {
+    window.location.href = `/app/#${window.location.pathname.replace('/app/', '')}`;
+  }
+}, {
+  immediate: true,
+});
 </script>
