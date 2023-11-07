6fd0b185c74d925d8caa622d9ceb0c2695027b97
Leonardo Matos <leomp120894@gmail.com>
2023-11-06 21:21:37 -0300

    Updating login form to handle submits throttling and custom link action URL

--- functions/ssr/src/components/LoginForm.vue
+++ functions/ssr/src/components/LoginForm.vue
@@ -1,7 +1,7 @@
 <template>
   <form
     class="text-base-800 w-full max-w-sm bg-white p-6 text-base md:p-8"
-    @submit.prevent="submitLogin"
+    @submit.prevent="() => submitLogin(loginLinkActionUrl)"
   >
     <slot name="head" />
     <p class="text-base-600 mb-4 text-2xl font-light">
@@ -28,24 +28,45 @@
       class="mb-0.5 mt-2 w-full rounded lowercase"
       required
     />
-    <a
-      v-show="!isSignUp"
-      href="?password"
-      class="ui-link text-base-500 text-right text-sm lowercase"
-      @click.prevent="isLinkSignIn = !isLinkSignIn"
-    >
-      {{ isLinkSignIn ? $t.i19enterWithPassword : $t.i19iForgotMyPassword }}
-    </a>
-    <button type="submit" class="ui-btn-lg ui-btn-primary mb-2 mt-5 w-full">
-      {{ isSignUp ? $t.i19signUp : $t.i19accessMyAccount }}
-    </button>
-    <a
-      href="?sign_up"
-      class="ui-btn-lg ui-btn-contrast block w-full text-center"
-      @click.prevent="isSignUp = !isSignUp"
-    >
-      {{ isSignUp ? $t.i19accessMyAccount : $t.i19createAnAccount }}
-    </a>
+    <div class="relative">
+      <div
+        class="transition-opacity"
+        :class="isEmailSentMsg && 'opacity-0 invisible'"
+      >
+        <a
+          v-show="!isSignUp && !isLinkSignInBlocked"
+          href="?password"
+          class="ui-link text-base-500 text-right text-sm lowercase"
+          @click.prevent="isLinkSignIn = !isLinkSignIn"
+        >
+          {{ isLinkSignIn ? $t.i19enterWithPassword : $t.i19iForgotMyPassword }}
+        </a>
+        <button
+          type="submit"
+          :disabled="isSubmitting"
+          class="ui-btn-lg ui-btn-primary mb-2 mt-5 w-full"
+          :class="isSubmitting && 'opacity-50'"
+        >
+          {{ isSignUp ? $t.i19signUp : $t.i19accessMyAccount }}
+        </button>
+      </div>
+      <Fade speed="slow">
+        <p
+          v-if="isEmailSentMsg"
+          class="text-success-800 absolute left-0 top-0 mt-4 font-medium"
+        >
+          <i class="i-check mr-1"></i>
+          {{ $t.i19emailWasSentMsg }}
+        </p>
+      </Fade>
+      <a
+        href="?sign_up"
+        class="ui-btn-lg ui-btn-contrast block w-full text-center"
+        @click.prevent="isSignUp = !isSignUp"
+      >
+        {{ isSignUp ? $t.i19accessMyAccount : $t.i19createAnAccount }}
+      </a>
+    </div>
   </form>
 </template>
 
@@ -53,11 +74,34 @@
 import useLoginForm from '@@sf/composables/use-login-form';
 import { customerName } from '@@sf/state/customer-session';
 
+export interface Props {
+  loginLinkActionUrl?: string | null;
+}
+
+defineProps<Props>();
 const {
   isLinkSignIn,
   isSignUp,
   email,
   password,
+  isSubmitting,
   submitLogin,
 } = useLoginForm();
+const isEmailSentMsg = ref(false);
+const countEmailsSent = ref(0);
+const isLinkSignInBlocked = computed(() => {
+  return countEmailsSent.value > 9;
+});
+watch((isSubmitting), () => {
+  if (!isSubmitting.value && isLinkSignIn.value) {
+    isEmailSentMsg.value = true;
+    countEmailsSent.value += 1;
+  }
+});
+watch([email, isSignUp], () => {
+  isEmailSentMsg.value = false;
+});
+watch(isLinkSignInBlocked, () => {
+  isLinkSignIn.value = false;
+});
 </script>
