422b277e6974024156102e083b0d429ae2e83727
Leonardo Matos <leomp120894@gmail.com>
2023-11-08 14:18:22 -0300

    Separated `<FooterStamps>` component to be resued on checkout SPA
    Remove duplicated address on shop footer

---
+++ functions/ssr/src/components/FooterStamps.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="ui-section">
+    <div class="flex flex-wrap items-start justify-between gap-5 lg:flex-nowrap">
+      <ul class="mx-auto flex items-center gap-x-6 gap-y-3
+        overflow-x-auto md:mx-0 md:flex-wrap md:overflow-hidden lg:gap-x-8">
+        <li v-for="(stamp, i) in stamps" :key="i">
+          <ALink :href="stamp.href?.replace('{domain}', $settings.domain || '')">
+            <slot :name="`picture-${i}`" />
+            <span v-if="!stamp.img" class="flex items-center">
+              <span
+                v-if="stamp.icon"
+                class="[&>*]:from-success-200 [&>*]:to-success-700
+                mr-2 text-4xl [&>*]:bg-gradient-to-br"
+              >
+                <i v-if="stamp.icon === 'lock'" class="i-lock-closed"></i>
+                <i v-else-if="stamp.icon === 'check'" class="i-check-badge"></i>
+                <i v-else class="i-arrow-path-rounded-square"></i>
+              </span>
+              <span class="text-base-600 max-w-[140px] text-sm font-medium">
+                {{ stamp.alt }}
+                <i
+                  v-if="stamp.href && stamp.href.charAt(0) !== '/'"
+                  class="bg-base-400 i-arrow-top-right-on-square ml-0.5"
+                ></i>
+              </span>
+            </span>
+          </ALink>
+        </li>
+      </ul>
+      <div class="mx-auto flex flex-wrap items-center justify-end
+        gap-4 overflow-x-auto text-2xl md:mx-0 md:flex-nowrap md:overflow-hidden">
+        <PaymentMethodFlag
+          v-for="paymentMethod in $settings.paymentMethods"
+          :key="paymentMethod"
+          :flag="paymentMethod"
+        />
+      </div>
+    </div>
+    <div class="mt-7 justify-between gap-4
+      text-center text-xs md:flex md:text-left">
+      <div class="mb-3 md:mb-0">
+        @ {{ new Date().getFullYear() }} {{ $settings.corporateName }}
+        {{ $settings.address ? `/ ${$settings.address}` : '' }}
+        / {{ $settings.docNumber }}
+      </div>
+      <ALink href="https://www.ecomplus.io/" class="italic text-[#37003c]">
+        powered by <b>E-Com Plus</b>
+      </ALink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { LayoutContent } from '@@sf/content';
+import PaymentMethodFlag from '@@sf/components/PaymentMethodFlag.vue';
+
+export interface Props {
+  stamps?: LayoutContent['footer']['stamps'];
+}
+
+defineProps<Props>();
+</script>
--- functions/ssr/src/components/ShopFooter.vue
+++ functions/ssr/src/components/ShopFooter.vue
@@ -19,10 +19,7 @@
               </SocialNetworkLink>
             </span>
           </div>
-          <div v-if="$settings.address || $settings.phone" class="text-base-700 mt-3">
-            <address v-if="$settings.address" class="mr-4 inline-block">
-              {{ $settings.address }}
-            </address>
+          <div v-if="$settings.phone" class="text-base-700 mt-3">
             <ALink
               v-if="$settings.phone"
               :href="`tel:${$settings.phone.replace(/\D/g, '')}`"
@@ -72,55 +69,7 @@
         </div>
       </div>
     </div>
-    <div class="ui-section">
-      <div class="flex flex-wrap items-start justify-between gap-5 lg:flex-nowrap">
-        <ul class="mx-auto flex items-center gap-x-6 gap-y-3
-          overflow-x-auto md:mx-0 md:flex-wrap md:overflow-hidden lg:gap-x-8">
-          <li v-for="(stamp, i) in stamps" :key="i">
-            <ALink :href="stamp.href?.replace('{domain}', $settings.domain || '')">
-              <slot :name="`picture-${i}`" />
-              <span v-if="!stamp.img" class="flex items-center">
-                <span
-                  v-if="stamp.icon"
-                  class="[&>*]:from-success-200 [&>*]:to-success-700
-                  mr-2 text-4xl [&>*]:bg-gradient-to-br"
-                >
-                  <i v-if="stamp.icon === 'lock'" class="i-lock-closed"></i>
-                  <i v-else-if="stamp.icon === 'check'" class="i-check-badge"></i>
-                  <i v-else class="i-arrow-path-rounded-square"></i>
-                </span>
-                <span class="text-base-600 max-w-[140px] text-sm font-medium">
-                  {{ stamp.alt }}
-                  <i
-                    v-if="stamp.href && stamp.href.charAt(0) !== '/'"
-                    class="bg-base-400 i-arrow-top-right-on-square ml-0.5"
-                  ></i>
-                </span>
-              </span>
-            </ALink>
-          </li>
-        </ul>
-        <div class="mx-auto flex flex-wrap items-center justify-end
-          gap-4 overflow-x-auto text-2xl md:mx-0 md:flex-nowrap md:overflow-hidden">
-          <PaymentMethodFlag
-            v-for="paymentMethod in $settings.paymentMethods"
-            :key="paymentMethod"
-            :flag="paymentMethod"
-          />
-        </div>
-      </div>
-      <div class="mt-7 justify-between gap-4
-        text-center text-xs md:flex md:text-left">
-        <div class="mb-3 md:mb-0">
-          @ {{ new Date().getFullYear() }} {{ $settings.corporateName }}
-          {{ $settings.address ? `/ ${$settings.address}` : '' }}
-          / {{ $settings.docNumber }}
-        </div>
-        <ALink href="https://www.ecomplus.io/" class="italic text-[#37003c]">
-          powered by <b>E-Com Plus</b>
-        </ALink>
-      </div>
-    </div>
+    <FooterStamps :stamps="stamps" />
   </footer>
 </template>
 
@@ -129,7 +78,7 @@ import type { Categories } from '@cloudcommerce/api/types';
 import type { LayoutContent } from '@@sf/content';
 import { socialNetworks } from '@@sf/sf-lib';
 import SocialNetworkLink from '@@sf/components/SocialNetworkLink.vue';
-import PaymentMethodFlag from '@@sf/components/PaymentMethodFlag.vue';
+import FooterStamps from '~/components/FooterStamps.vue';
 
 export interface Props {
   stamps?: LayoutContent['footer']['stamps'];
