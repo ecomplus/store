e3fa5d24d5b245b7e2ed55ad54bb08548c4cd25b
Leonardo Matos <leomp120894@gmail.com>
2023-11-08 21:12:53 -0300

    Setup checkout page as component and fix brand colors for vbeta SPA

---
+++ functions/ssr/src/components/CheckoutPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <article class="flex min-h-screen flex-col justify-end">
+    <section class="grow">
+      <article id="storefront-app"></article>
+    </section>
+    <section class="bg-base-50 mt-4 lg:mt-8">
+      <nav class="ui-section-slim">
+        <ul class="mt-6 flex flex-wrap justify-center gap-6 lg:gap-8">
+          <li
+            v-for="({ title, href }, i) in $settings.serviceLinks"
+            :key="`s-${i}`"
+          >
+            <ALink :href="href" target="blank" class="ui-link text-base-700">
+              {{ title }}
+              <i class="bg-base-300 i-arrow-top-right-on-square ml-0.5"></i>
+            </ALink>
+          </li>
+        </ul>
+      </nav>
+      <FooterStamps :stamps="footerStamps" />
+    </section>
+  </article>
+</template>
+
+<script setup lang="ts">
+import FooterStamps from '~/components/FooterStamps.vue';
+
+export interface Props {
+  footerStamps: InstanceType<typeof FooterStamps>['$props']['stamps'];
+}
+
+defineProps<Props>();
+</script>
