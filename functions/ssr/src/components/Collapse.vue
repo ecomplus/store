f656609f42dc71a99dbde821056fbffbe6d38bda
Leonardo Matos <leomp120894@gmail.com>
2023-12-28 20:07:47 -0300

    Prefer using chevron/arrow icons _right_ only
    As https://github.com/ecomplus/cloud-commerce/commit/247c3496a39003706dab02231fe4455cd3c1283b
    
    [skip ci] with other breacking changes

--- functions/ssr/src/components/Collapse.vue
+++ functions/ssr/src/components/Collapse.vue
@@ -1,9 +1,9 @@
 <template>
-  <details class="group my-4 md:my-5 [&_i]:open:rotate-180">
+  <details class="group my-4 md:my-5 [&_i]:open:-rotate-90">
     <summary role="button" class="text-base-700 hover:text-base-800
       [&>i]:hover:text-primary list-none text-2xl font-medium
       lowercase group-open:mb-3">
-      <i class="i-chevron-down mr-1"></i>
+      <i class="i-chevron-right mr-1 rotate-90"></i>
       <span>{{ title }}</span>
     </summary>
     <slot />
