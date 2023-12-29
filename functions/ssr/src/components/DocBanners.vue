1ba21f39b2712ab2f3406946322e900359511ffa
Leonardo Matos <leomp120894@gmail.com>
2023-12-29 03:08:51 -0300

    New `<DocBanners>` component and section [skip ci]

---
+++ functions/ssr/src/components/DocBanners.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="ui-section">
+    <ul class="flex flex-wrap gap-4 lg:flex-nowrap">
+      <li
+        v-for="(picture, i) in pictures" :key="i"
+        class="lg:flex-1 [&_img]:w-full [&_img]:object-cover"
+      >
+        <AImg :picture="picture" />
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script setup lang="ts">
+import type { Categories, Brands, Collections } from '@cloudcommerce/api/types';
+
+export interface Props {
+  apiDoc?: Partial<Categories | Brands | Collections>;
+  title?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  apiDoc: () => globalThis.$storefront.apiContext?.doc as Categories,
+});
+const pictures = computed(() => props.apiDoc.pictures || []);
+</script>
