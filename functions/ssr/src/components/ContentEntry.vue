b8bb7924553753dbc4b35a266f3d85239fa88e3c
Leonardo Matos <leomp120894@gmail.com>
2024-01-03 19:53:26 -0300

    New content entry component rendering MD to HTML
    From sections, with https://github.com/ecomplus/cloud-commerce/commit/54692789aaa1f5c84a6648ee98585d6dd5911360 [skip ci]

---
+++ functions/ssr/src/components/ContentEntry.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="ui-section">
+    <article class="prose">
+      <h1 class="ui-title not-prose my-4">{{ title }}</h1>
+      <ContentClearfix :html="html" />
+    </article>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { micromark } from 'micromark';
+import ContentClearfix from '@@sf/components/ContentClearfix.vue';
+
+export interface Props {
+  title: string;
+  markdown: string;
+}
+
+const props = defineProps<Props>();
+const html = computed(() => micromark(props.markdown, {
+  allowDangerousHtml: true,
+}));
+</script>
