e0ebd6a23dc73ea868b77fb7dab7a29fd53c0856
Leonardo Matos <leomp120894@gmail.com>
2024-02-24 18:30:05 -0300

    Prevent warnings with undeclared `cmsPreview` prop on product details
    In addition to https://github.com/ecomplus/cloud-commerce/commit/2b6eec4ad101c252a131de30dbc9fcd9d97dc1d7 [skip ci]

--- functions/ssr/src/components/ProductDetails.vue
+++ functions/ssr/src/components/ProductDetails.vue
@@ -1,14 +1,14 @@
 <template>
-  <section class="ui-section relative grid grid-cols-1 items-start
-    gap-x-5 lg:grid-cols-4 lg:gap-x-4 2xl:gap-x-6">
+  <section class="relative grid grid-cols-1 items-start gap-x-5
+    ui-section lg:grid-cols-4 lg:gap-x-4 2xl:gap-x-6">
     <div class="w-full lg:col-span-3">
       <template v-if="product.pictures?.length">
         <ImagesGallery :pictures="product.pictures" class="mb-5" />
       </template>
     </div>
-    <div class="lg:sticky-header:translate-y-14 top-0 pt-4
-      transition-transform lg:sticky lg:pb-4">
-      <h1 class="text-secondary-900 ui-text-brand text-lg">
+    <div class="top-0 pt-4 transition-transform
+      lg:sticky lg:pb-4 lg:sticky-header:translate-y-14">
+      <h1 class="text-lg text-secondary-900 ui-text-brand">
         {{ title }}
       </h1>
       <div v-if="isActive" class="mt-5">
@@ -20,9 +20,9 @@
           class="my-4"
         />
         <Fade slide="down">
-          <div v-if="hasSkuSelectionAlert" class="ui-alert inline-block">
+          <div v-if="hasSkuSelectionAlert" class="inline-block ui-alert">
             {{ $t.i19selectVariationMsg }}
-            <i class="i-arrow-right ml-1 -rotate-90"></i>
+            <i class="ml-1 -rotate-90 i-arrow-right"></i>
           </div>
         </Fade>
         <div class="mt-5 flex flex-wrap items-center gap-x-4 gap-y-2
@@ -31,11 +31,11 @@
             v-model="quantity"
             :min="product.min_quantity"
             :max="product.quantity"
-            class="border-base-100 rounded md:mr-5
+            class="rounded border-base-100 md:mr-5
             lg:mb-2 lg:mr-auto lg:border-2"
           />
           <CheckoutLink
-            class="ui-btn-lg ui-btn-primary grow text-center"
+            class="grow text-center ui-btn-lg ui-btn-primary"
             to="checkout"
             :cart-item="{
               product_id: product._id,
@@ -45,19 +45,19 @@
             :data-tooltip="!isSkuSelected ? $t.i19chooseProductDetailsToBuy : null"
             @click="checkVariation"
           >
-            <i class="i-chevron-double-right mr-1"></i>
+            <i class="mr-1 i-chevron-double-right"></i>
             {{ $t.i19buy }}
           </CheckoutLink>
           <button
-            class="ui-btn-lg ui-btn-contrast grow"
+            class="grow ui-btn-lg ui-btn-contrast"
             @click.prevent="addToCart"
             :data-tooltip="!isSkuSelected ? $t.i19chooseProductDetailsToBuy : null"
           >
             {{ $t.i19addToCart }}
           </button>
         </div>
-        <div class="border-base-50 border-t-base-100 mt-6
-          rounded border-2 p-4 lg:mt-4">
+        <div class="mt-6 rounded border-2
+          border-base-50 border-t-base-100 p-4 lg:mt-4">
           <ShippingCalculator
             :shipped-items="[{ ...product, quantity }]"
             has-label
@@ -73,6 +73,7 @@
 
 <script setup lang="ts">
 import type { ResourceId, Products } from '@cloudcommerce/api/types';
+import type { SectionPreviewProps } from '@@sf/state/use-cms-preview';
 import { useUrlSearchParams } from '@vueuse/core';
 import { addProductToCart } from '@@sf/state/shopping-cart';
 import { useProductCard } from '@@sf/composables/use-product-card';
@@ -83,7 +84,7 @@ import ImagesGallery from '~/components/ImagesGallery.vue';
 import SkuSelector from '~/components/SkuSelector.vue';
 import ShippingCalculator from '~/components/ShippingCalculator.vue';
 
-export interface Props {
+export interface Props extends Partial<SectionPreviewProps> {
   product?: Products;
 }
 
