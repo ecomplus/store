a8b9073dcae6182f8e97217769eebd53fde8c7ac
Leonardo Matos <leomp120894@gmail.com>
2024-04-02 17:19:17 -0300

    Removing `vite-plugin-pwa` from tsconfig

--- functions/ssr/src/env.d.ts
+++ functions/ssr/src/env.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="../content/types" />
 /// <reference types="astro/client" />
-/// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vue/ref-macros" />
 /// <reference types="@cloudcommerce/storefront/client" />
 /// <reference types="@cloudcommerce/storefront/server" />
