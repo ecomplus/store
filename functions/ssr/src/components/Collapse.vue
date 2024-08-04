4f54285c02830adcd246fb85476fd362ecc36706
Leonardo Matos <leomp120894@gmail.com>
2024-08-04 13:22:12 -0300

    Minor fix for chevron icons on Collapse component [skip ci]

--- functions/ssr/src/components/Collapse.vue
+++ functions/ssr/src/components/Collapse.vue
@@ -1,9 +1,9 @@
 <template>
-  <details class="group my-4 md:my-5 [&_i]:open:-rotate-90">
-    <summary role="button" class="text-base-700 hover:text-base-800
-      [&>i]:hover:text-primary list-none text-2xl font-medium
-      lowercase group-open:mb-3">
-      <i class="i-chevron-right mr-1 rotate-90"></i>
+  <details class="group my-4 md:my-5 [&>summary_i]:open:-rotate-90">
+    <summary role="button" class="list-none text-2xl
+      font-medium lowercase text-base-700 hover:text-base-800
+      group-open:mb-3 [&>i]:hover:text-primary">
+      <i class="mr-1 rotate-90 i-chevron-right"></i>
       <span>{{ title }}</span>
     </summary>
     <slot />
