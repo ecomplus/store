543ac6023109c07e5ca88d9b6335bd5dd9cac464
Leonardo Matos <leomp120894@gmail.com>
2025-04-08 14:55:14 -0300

    Testing custom Astro/Vite config to fix unique `hoisted` output script filenames

--- functions/ssr/astro.config.mjs
+++ functions/ssr/astro.config.mjs
@@ -1,6 +1,29 @@
+import { basename } from 'node:path';
 import { defineConfig } from 'astro/config';
 import { astroConfig } from '@cloudcommerce/storefront/astro.config.mjs';
 
 astroConfig.devToolbar = { enabled: false };
+
+const buildConfig = astroConfig.build;
+astroConfig.vite.build = {
+  rollupOptions: {
+    output: {
+      chunkFileNames: `${buildConfig.assets}/[name].[hash].js`,
+      assetFileNames: `${buildConfig.assets}/[name].[hash][extname]`,
+      entryFileNames: (chunkInfo) => {
+        if (chunkInfo.name.includes('hoisted')) {
+          const sfScripts = chunkInfo.moduleIds?.filter((id) => {
+            return id.includes('storefront/src/lib/scripts/');
+          });
+          if (sfScripts?.length === 1) {
+            const scriptName = basename(sfScripts[0], '.ts').replaceAll('.', '');
+            return `${buildConfig.assets}/[name]-${scriptName}.[hash].js`;
+          }
+        }
+        return `${buildConfig.assets}/[name].[hash].js`;
+      },
+    },
+  },
+};
 
 export default defineConfig(astroConfig);
