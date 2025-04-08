546088a2a1a2a91a15f86fc1ca4b5698b863775c
Leonardo Matos <leomp120894@gmail.com>
2025-04-08 15:30:34 -0300

    Reverting (test) Astro custom outputs config

--- functions/ssr/astro.config.mjs
+++ functions/ssr/astro.config.mjs
@@ -1,26 +1,6 @@
-import { basename } from 'node:path';
 import { defineConfig } from 'astro/config';
 import { astroConfig } from '@cloudcommerce/storefront/astro.config.mjs';
 
 astroConfig.devToolbar = { enabled: false };
-
-astroConfig.vite.build = {
-  rollupOptions: {
-    output: {
-      entryFileNames: (chunkInfo) => {
-        if (chunkInfo.name.includes('hoisted')) {
-          const sfScripts = chunkInfo.moduleIds?.filter((id) => {
-            return id.includes('storefront/src/lib/scripts/');
-          });
-          if (sfScripts?.length === 1) {
-            const scriptName = basename(sfScripts[0], '.ts').replaceAll('.', '');
-            return `_astro/[name]-${scriptName}.[hash].js`;
-          }
-        }
-        return `_astro/[name].[hash].js`;
-      },
-    },
-  },
-};
 
 export default defineConfig(astroConfig);
